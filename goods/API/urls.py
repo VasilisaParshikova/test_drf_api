0728194ccac2788ff663e873e7fe7fe9e060d893
Vasilisa <vasilisazaykova@gmail.com>
2024-09-25 23:23:48 +0400

    base functions

--- goods/API/urls.py
+++ goods/API/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import ProductViewSet
+from .views import ProductViewSet, BrandViewSet
 
 router = DefaultRouter()
 router.register(r'goods', ProductViewSet, basename='product')
+router.register(r'brands', BrandViewSet, basename='drand')
 
 urlpatterns = [
     path('', include(router.urls)),
--- goods/API/views.py
+++ goods/API/views.py
@@ -1,10 +1,17 @@
 from rest_framework import viewsets
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Brand
+from .serializers import ProductSerializer, BrandSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete']
+
+
+class BrandViewSet(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
 
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
